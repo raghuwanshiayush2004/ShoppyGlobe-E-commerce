1159d0327ef685e41c32dec0e521a370d9e49a37
ayush raghuwanshi <raghuwanshi1405@gmail.com>
2025-11-18 02:18:59 +0530

    add link go back

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -10,6 +10,9 @@ const NotFound = () => {
           <h1>404</h1>
           <h2>Page Not Found</h2>
           <p>The page you are looking for doesn't exist or has been moved.</p>
+          <Link to="/" className="home-link">
+            Go Back Home
+          </Link>
 
         </div>
       </div>
