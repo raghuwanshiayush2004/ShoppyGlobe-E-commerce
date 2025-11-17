024b28a3101c507f9c144ffa217957345397db35
ayush raghuwanshi <raghuwanshi1405@gmail.com>
2025-11-18 02:17:52 +0530

    set page not found

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,7 +7,8 @@ const NotFound = () => {
     <div className="not-found">
       <div className="container">
         <div className="not-found-content">
-
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
           <p>The page you are looking for doesn't exist or has been moved.</p>
 
         </div>
